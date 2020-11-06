9c196a05915d7df1d51bd7bce1428347754a5b8e
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-11-06 14:55:17 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,7 @@
 class PostsController < ApplicationController
 
   def index 
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
+    @posts = Post.all.order(id: "DESC")
   end
 
 end
